e02d855b4bc21b33c8ecbc1470be74e6aced3295
saurabhpathak <saurabhpathak072@gmail.com>
2024-02-12 13:28:09 +0530

    updated the log message

--- server/Controller/post.js
+++ server/Controller/post.js
@@ -4,17 +4,25 @@ import { appendLog } from '../../utils/logs.js';
 
 export const getPost = async (req, res) => {
   try {
-     // Log request details
-     const requestLog = `${new Date().toISOString()} - ${req.method} ${req.url} - Request Body: ${JSON.stringify(req.body)}\n`;
+    // Log request details
+    const requestLog = `${new Date().toISOString()} - ${req.method} ${
+      req.url
+    } - Request Body: ${JSON.stringify(req.body)}\n`;
 
-     // Append request log to Azure Blob Storage
-     await appendLog(requestLog);
- 
+    // Append request log to Azure Blob Storage
+    await appendLog(requestLog);
+
     const postList = await PostModel.find({});
-    res.status(200).json({
+
+    const response = {
       message: 'GetPost',
       data: postList,
-    });
+    };
+
+    const responseLog = `Response Body: ${JSON.stringify(response)}\n\n`;
+    await appendLog(responseLog);
+
+    res.status(200).json(response);
   } catch (error) {
     console.log(chalk.red.bold.italic(error));
     const errorMessage = new Error(error);
@@ -26,6 +34,13 @@ export const getPost = async (req, res) => {
 };
 
 export const createPost = async (req, res) => {
+  const requestLog = `${new Date().toISOString()} - ${req.method} ${
+    req.url
+  } - Request Body: ${JSON.stringify(req.body)}\n`;
+
+  // Append request log to Azure Blob Storage
+  await appendLog(requestLog);
+
   const { title, postMessage } = req.body;
   const createPost = new PostModel({
     title,
@@ -33,20 +48,37 @@ export const createPost = async (req, res) => {
   });
 
   const createPostResult = await createPost.save();
-  console.log('createPostResult', createPostResult);
-  res.status(201).json({
+  const response = {
+    message: 'Post Created',
     data: createPostResult,
-  });
+  };
+
+  const responseLog = `Response Body: ${JSON.stringify(response)}\n\n`;
+  await appendLog(responseLog);
+
+  res.status(201).json(response);
 };
 
 export const deletePost = async (req, res) => {
+  const requestLog = `${new Date().toISOString()} - ${req.method} ${
+    req.url
+  } - Request Body: ${JSON.stringify(req.body)}\n`;
+
+  // Append request log to Azure Blob Storage
+  await appendLog(requestLog);
+
   const { postId } = req.params;
   try {
     const deletedPost = await PostModel.findByIdAndDelete(postId);
-    res.status(200).json({
+    const response = {
       message: 'Post deleted',
       data: deletedPost,
-    });
+    };
+  
+    const responseLog = `Response Body: ${JSON.stringify(response)}\n\n`;
+    await appendLog(responseLog);
+
+    res.status(200).json(response);
   } catch (error) {
     console.log(chalk.red.bold.italic(error));
     const errorMessage = new Error(error);
@@ -58,35 +90,49 @@ export const deletePost = async (req, res) => {
 };
 
 export const updatePost = async (req, res) => {
+  const requestLog = `${new Date().toISOString()} - ${req.method} ${
+    req.url
+  } - Request Body: ${JSON.stringify(req.body)}\n`;
+
+  // Append request log to Azure Blob Storage
+  await appendLog(requestLog);
+
   const { postId } = req.params;
   const data = req.body;
-  console.log('req.body',data);
+  
   try {
     const findPost = await PostModel.findById(postId).select(
       'title postMessage id'
     );
- 
+
     if (findPost) {
-     console.log('data',data);
-      const updatePost = await PostModel.findByIdAndUpdate(postId, {...data}, {new:true});
+      console.log('data', data);
+      const updatePost = await PostModel.findByIdAndUpdate(
+        postId,
+        { ...data },
+        { new: true }
+      );
       console.log(chalk.blue(updatePost));
-      res.status(200).json({
+      const response = {
         message: 'Post update',
         data: updatePost,
-      });
-     
-    }
-    else{
-      throw(new Error("Post not exist!!"))
+      };
+    
+      const responseLog = `Response Body: ${JSON.stringify(response)}\n\n`;
+      await appendLog(responseLog);
+  
+      res.status(200).json(response);
+    } else {
+      throw new Error('Post not exist!!');
     }
   } catch (error) {
     console.log(chalk.red.bold.italic(error));
     const errorMessage = error;
     res.status(500).json({
       message: 'Internal server error',
-      error:{
-        message:error.message,
-        stack:error.stack
+      error: {
+        message: error.message,
+        stack: error.stack,
       },
       errorMessage,
     });
--- utils/logs.js
+++ utils/logs.js
@@ -1,45 +1,43 @@
-import express from 'express';
 import axios from 'axios';
-import fs from 'fs';
-import chalk from 'chalk';
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+import chalk from 'chalk';
 
 // Azure Storage Account information
 const accountName = 'tablestoragecoursera';
 const containerName = 'myblobcontainer';
 const blobName = 'api-logs.txt';
-const accountKey = '3+5Vs7R9NnGcyt+mZsMt+HHGOSfqfCyi9p8YEV+PkI6n2zd/3uzWEeS60i6GyjSat5NJY2NsaZW7+AStfQ1k0Q=='; // or use a SAS token
+
+const accountKey = process.env.AZURE_ACCOUNT_KEY; // or use a SAS token
 
 // Azure Blob Storage service endpoint
 const blobServiceEndpoint = `https://${accountName}.blob.core.windows.net`;
 
-// URL to the blob
-const blobUrl = `${blobServiceEndpoint}/${containerName}/${blobName}`;
-
-
-
-
-
-
 // Function to append logs to the blob
-export const appendLog=async (log)=> {
+export const appendLog = async (log) => {
+  const sasKey = process.env.AZURE_SAS_KEY;
+  // URL to the blob
+  const blobUrl = `${blobServiceEndpoint}/${containerName}/${blobName}${sasKey}`;
   try {
     // Get the current content of the blob
     const { data } = await axios.get(blobUrl, {
-      headers: { 'x-ms-version': '2019-07-07', 'x-ms-blob-type': 'BlockBlob', Authorization: `SharedKey ${accountName}:${accountKey}` },
+      headers: {
+        // 'x-ms-version': '2019-07-07',
+        'x-ms-blob-type': 'BlockBlob',
+        // Authorization: `SharedKey ${accountName}:${accountKey}`,
+      },
     });
 
     // Append the log to the current content
     const updatedContent = data + log;
-
     // Upload the updated content back to the blob
     await axios.put(blobUrl, updatedContent, {
-      headers: { 'x-ms-blob-type': 'BlockBlob', Authorization: `SharedKey ${accountName}:${accountKey}` },
+      headers: {
+        'x-ms-blob-type': 'BlockBlob',
+        // Authorization: `SharedKey ${accountName}:${accountKey}`,
+      },
     });
   } catch (error) {
     console.log(chalk.red(error));
     console.error('Error appending log to blob:', error.message);
   }
-}
+};
